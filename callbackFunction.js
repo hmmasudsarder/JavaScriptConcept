aa4bcd531e8db909361578385a6eebf0bc291895
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-09-05 04:42:33 +0600

    callback function

---
+++ callbackFunction.js
@@ -0,0 +1,13 @@
+function fetchData(callback) {
+    console.log("Fetching data...");
+    setTimeout(() => {
+      console.log("Data fetched.");
+      callback(); // Callback function is executed
+    }, 2000);
+  }
+  
+  function processData() {
+    console.log("Processing data...");
+  }
+  
+  fetchData(processData); // Passing processData as the callback functionj
